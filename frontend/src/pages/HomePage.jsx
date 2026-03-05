f9958a7450131ecea0eadf91372178206f1525c6
naveenagrawalgit <naveenagrawal1008@gmail.com>
2026-03-05 10:51:18 +0530

    image upload

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='bg-gradient-to-br from-base-100 via-base-200 to-base-300' >
+        {/* NAVBAR */}
+        <nav className='bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg' >
+
+        <div className='max-w-7xl mx-auto px-4 py-4'>
+
+        </div>
+        </nav>
+
+
+
+
+
+
+    </div>
   )
 }
 
