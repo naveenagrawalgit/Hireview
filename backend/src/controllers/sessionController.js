885cf5035fddf0e8df61bfc9b20f4d0eefe74bf7
naveenagrawalgit <naveenagrawal1008@gmail.com>
2026-03-05 10:54:17 +0530

    import fix

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,5 +1,5 @@
 import { chatClient, streamClient } from "../lib/stream.js";
-import { Session } from "../models/Session.js";
+import  Session  from "../models/Session.js";
 
 export async function createSession(req,res){
 
