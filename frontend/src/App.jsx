3f14cb3f6755295f0abcf3c34de1f2d7f2fa444e
naveenagrawalgit <naveenagrawal1008@gmail.com>
2026-03-12 18:12:30 +0530

    session page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import ProblemsPage from './pages/ProblemsPage';
 import { Toaster } from 'react-hot-toast';
 import DashboradPage from './pages/DashboradPage';
 import ProblemPage from './pages/ProblemPage';
+import SessionPage from './pages/SessionPage';
 
 function App() {
   
@@ -21,8 +22,11 @@ function App() {
    <Route path="/" element ={ !isSignedIn? <HomePage/> : <Navigate to={"/dashboard"}/> } />
    <Route path="/dashboard" element ={isSignedIn? <DashboradPage/> : <Navigate to={"/"}/> } />
    <Route path='/about' element={<AboutPage/>} />
+
    <Route path='/problems' element={isSignedIn? <ProblemsPage/> : <Navigate to={"/"}/> } />
    <Route path='/problem/:id' element={isSignedIn? <ProblemPage/> : <Navigate to={"/"}/> } />
+   <Route path='/session/:id' element={isSignedIn? <SessionPage/> : <Navigate to={"/"}/> } />
+
   
 
 
